8a0cf8ccc56754bf5d399c9745f7bd23e97203dc
Pelf <fry1988@mail.ru>
2012-10-28 01:11:44 +0400

    [48] База / Заклинания: исправлен порядок отображения гильдейских навыков.

---
+++ sql/updates/circle_updates/world/48_world_spell_ranks.sql
@@ -0,0 +1,12 @@
+INSERT INTO `spell_ranks` (`first_spell_id`, `spell_id`, `rank`) VALUES (78631, 78631, 1);
+INSERT INTO `spell_ranks` (`first_spell_id`, `spell_id`, `rank`) VALUES (78631, 78632, 2);
+INSERT INTO `spell_ranks` (`first_spell_id`, `spell_id`, `rank`) VALUES (83942, 83942, 1);
+INSERT INTO `spell_ranks` (`first_spell_id`, `spell_id`, `rank`) VALUES (83942, 83943, 2);
+INSERT INTO `spell_ranks` (`first_spell_id`, `spell_id`, `rank`) VALUES (78634, 78634, 1);
+INSERT INTO `spell_ranks` (`first_spell_id`, `spell_id`, `rank`) VALUES (78634, 78635, 2);
+INSERT INTO `spell_ranks` (`first_spell_id`, `spell_id`, `rank`) VALUES (83940, 83940, 1);
+INSERT INTO `spell_ranks` (`first_spell_id`, `spell_id`, `rank`) VALUES (83940, 83941, 2);
+INSERT INTO `spell_ranks` (`first_spell_id`, `spell_id`, `rank`) VALUES (83959, 83959, 1);
+INSERT INTO `spell_ranks` (`first_spell_id`, `spell_id`, `rank`) VALUES (83959, 83960, 2);
+INSERT INTO `spell_ranks` (`first_spell_id`, `spell_id`, `rank`) VALUES (83945, 83945, 1);
+INSERT INTO `spell_ranks` (`first_spell_id`, `spell_id`, `rank`) VALUES (83945, 83961, 2);
