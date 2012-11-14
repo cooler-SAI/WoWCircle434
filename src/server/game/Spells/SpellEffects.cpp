1c30f9d84aef2604e31ba0d08cd979765e407b2a
Heisenberg <Heisenberg@Heisenberg-0x40.domain>
2012-11-14 21:08:54 +0200

    [232] Ядро / Заклинания: Исправлено заклинание Воскрешение Мертвых Исправлен радиус заклинания Ненасытная стужа

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -976,29 +976,12 @@ void Spell::EffectDummy(SpellEffIndex effIndex)
 
                     // Do we have talent Master of Ghouls?
                     if (m_caster->HasAura(52143))
-                        // summon as pet
-                        bp = 52150;
+                        spell_id = 52150; // summon as pet
                     else
-                        // or guardian
-                        bp = 46585;
+                        spell_id = 46585; // or guardian
 
-                    if (m_targets.HasDst())
-                        targets.SetDst(*m_targets.GetDstPos());
-                    else
-                    {
-                        targets.SetDst(*m_caster);
-                        // Corpse not found - take reagents (only not triggered cast can take them)
-                        triggered = false;
-                    }
                     // Remove cooldown - summon spellls have category
                     m_caster->ToPlayer()->RemoveSpellCooldown(m_spellInfo->Id, true);
-                    spell_id = 48289;
-                    break;
-                // Raise dead - take reagents and trigger summon spells
-                case 48289:
-                    if (m_targets.HasDst())
-                        targets.SetDst(*m_targets.GetDstPos());
-                    spell_id = CalculateDamage(0, NULL);
                     break;
             }
             break;
--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -3011,10 +3011,10 @@ void SpellMgr::LoadDbcDataCorrections()
                 spellInfo->Effects[0].BasePoints = 1;
                 break;
             case 99:
-            case 1160:
             case 5857:
             case 15237:
             case 23455:
+            case 49203:
             case 52212:
                 spellInfo->Effects[0].SetRadiusIndex(13);
                 break;
