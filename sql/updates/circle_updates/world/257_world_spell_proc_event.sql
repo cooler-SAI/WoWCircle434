06cb60191eefdad9e809362b1c6c97983fdb5307
Pelf <fry1988@mail.ru>
2012-11-15 18:47:57 +0400

    [257] Ядро / Заклинания / Воин: исправлен прок таланта "Мания крови" (Blood Craze).

---
+++ sql/updates/circle_updates/world/257_world_spell_proc_event.sql
@@ -0,0 +1,3 @@
+UPDATE `spell_proc_event` SET `proc_ex`=0, `CustomChance`=10 WHERE `entry`=16487;
+UPDATE `spell_proc_event` SET `proc_ex`=0, `CustomChance`=10 WHERE `entry`=16489;
+UPDATE `spell_proc_event` SET `proc_ex`=0, `CustomChance`=10 WHERE `entry`=16492;
