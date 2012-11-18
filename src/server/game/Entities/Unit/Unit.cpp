1a0380cdb8edc5520c0474df604462527ba17444
crawling <athreNd@hotmail.com>
2012-11-19 07:11:57 +0800

    [360] Ядро / Заклинания / Маг: исправлен критический шанс таланта "Обледенение" (Frozen)

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -10547,6 +10547,7 @@ bool Unit::isSpellCrit(Unit* victim, SpellInfo const* spellProto, SpellSchoolMas
                         if (spellProto->SpellFamilyFlags[0] == 0x2 && spellProto->SpellIconID == 12)
                             if (victim->HasAuraWithMechanic((1<<MECHANIC_STUN) | (1<<MECHANIC_KNOCKOUT)))
                                 if (AuraEffect const* aurEff = GetAuraEffect(56369, EFFECT_0))
+                                    crit_chance += aurEff->GetAmount();
                         // Shatter (same misc values now)
                         if (victim->HasAuraState(AURA_STATE_FROZEN, spellProto, this))
                         {
