3862fbdf3cfb2e18edbfa28db6eeffc722a901fe
crawling <athreNd@hotmail.com>
2012-11-19 09:41:10 +0800

    [367] Ядро / Заклинания / Маг: исправления к урону "Живая Бомба" (Living Bomb) и проку "Путь Огня" (Hot Streak)

---
+++ sql/updates/circle_updates/world/367_spell_proc_event.sql
@@ -0,0 +1,3 @@
+-- (44445) Hot Streak (Rank 1)
+DELETE FROM `spell_proc_event` WHERE `entry` IN (44445);
+INSERT INTO `spell_proc_event` VALUES (44445, 0x00, 0x03, 0x00000013, 0x00001000, 0x00001000, 0x00011000, 0x00000002, 0, 100, 0);
