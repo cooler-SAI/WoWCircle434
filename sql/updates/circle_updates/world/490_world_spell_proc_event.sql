89268e34720378fa776018746f0a9d0bf1152f66
ungaminga <chloey@lavabit.com>
2012-12-03 18:51:09 +0200

    sql к [490] 8c55be9401a8

---
+++ sql/updates/circle_updates/world/490_world_spell_proc_event.sql
@@ -0,0 +1,3 @@
+-- (44445) Hot Streak (Rank 1)
+DELETE FROM `spell_proc_event` WHERE `entry` IN (44445);
+INSERT INTO `spell_proc_event` VALUES (44445, 0x00, 0x03, 0x00400013, 0x00001000, 0x00000000, 0x00010000, 0x00000002, 0, 33, 1);
