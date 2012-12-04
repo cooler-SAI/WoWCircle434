8854fc9dec9dac8651c384a2ea9b1d62c63949b5
Pelf <fry1988@mail.ru>
2012-12-04 17:35:28 +0300

    [510] Ядро / Заклинания / Чернокнижник: исправлен прок таланта "Тьма и пламя" (Shadow and Flame).

---
+++ sql/updates/circle_updates/world/510_world_spell_proc_event.sql
@@ -0,0 +1,6 @@
+DELETE FROM `spell_proc_event` WHERE `entry`=17793;
+DELETE FROM `spell_proc_event` WHERE `entry`=17796;
+DELETE FROM `spell_proc_event` WHERE `entry`=17801;
+INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES (17793, 0, 5, 1, 64, 0, 69968, 1027, 0, 0, 0);
+INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES (17796, 0, 5, 1, 64, 0, 69968, 1027, 0, 0, 0);
+INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES (17801, 0, 5, 1, 64, 0, 69968, 1027, 0, 0, 0);
