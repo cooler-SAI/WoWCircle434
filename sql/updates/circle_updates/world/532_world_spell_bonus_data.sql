5f9c85f2c2606c5929ff139eb8faadb6a5290b8f
Pelf <fry1988@mail.ru>
2012-12-06 00:45:12 +0300

    [532] Ядро / Заклинания / Друид: исправлен урон спеллов "Гнев" (Wrath), "Звездный огонь" (Starfire), "Звездный поток" (Starsurge), "Лунный огонь" (Moonfire), "Рой насекомых" (Insect Swarm).

---
+++ sql/updates/circle_updates/world/532_world_spell_bonus_data.sql
@@ -0,0 +1,14 @@
+DELETE FROM `spell_bonus_data` WHERE `entry`=5176;
+INSERT INTO `spell_bonus_data` (`entry`, `direct_bonus`, `dot_bonus`, `ap_bonus`, `ap_dot_bonus`, `comments`) VALUES (5176, 0.879, -1, -1, -1, 'Druid - Wrath');
+
+DELETE FROM `spell_bonus_data` WHERE `entry`=2912;
+INSERT INTO `spell_bonus_data` (`entry`, `direct_bonus`, `dot_bonus`, `ap_bonus`, `ap_dot_bonus`, `comments`) VALUES (2912, 1.231, -1, -1, -1, 'Druid - Starfire');
+
+DELETE FROM `spell_bonus_data` WHERE `entry`=78674;
+INSERT INTO `spell_bonus_data` (`entry`, `direct_bonus`, `dot_bonus`, `ap_bonus`, `ap_dot_bonus`, `comments`) VALUES (78674, 1.228, -1, -1, -1, 'Druid - Starsurge');
+
+DELETE FROM `spell_bonus_data` WHERE `entry`=5570;
+INSERT INTO `spell_bonus_data` (`entry`, `direct_bonus`, `dot_bonus`, `ap_bonus`, `ap_dot_bonus`, `comments`) VALUES (5570, -1, 0.13, -1, -1, 'Druid - Insect Swarm');
+
+DELETE FROM `spell_bonus_data` WHERE `entry`=8921;
+INSERT INTO `spell_bonus_data` (`entry`, `direct_bonus`, `dot_bonus`, `ap_bonus`, `ap_dot_bonus`, `comments`) VALUES (8921, 0.18, 0.14, -1, -1, 'Druid - Moonfire');
