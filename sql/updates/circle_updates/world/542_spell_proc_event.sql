ae77952cb777a27dc2f1762e45aa4c4d7693854a
crawling <athreNd@hotmail.com>
2012-12-06 11:53:37 +0800

    [542] Ядро / Заклинания / Маг: Исправлен прок "Затяжная зима" (Enduring Winter)

---
+++ sql/updates/circle_updates/world/542_spell_proc_event.sql
@@ -0,0 +1,11 @@
+-- (44561) Enduring Winter (Rank 1)
+DELETE FROM `spell_proc_event` WHERE `entry` IN (44561);
+INSERT INTO `spell_proc_event` VALUES (44561, 0x00, 0x03, 0x00000020, 0x00000000, 0x00000000, 0x00010000, 0x00000000, 0, 33, 0);
+
+-- (86500) Enduring Winter (Rank 2)
+DELETE FROM `spell_proc_event` WHERE `entry` IN (86500);
+INSERT INTO `spell_proc_event` VALUES (86500, 0x00, 0x03, 0x00000020, 0x00000000, 0x00000000, 0x00010000, 0x00000000, 0, 66, 0);
+
+-- (86508) Enduring Winter (Rank 3)
+DELETE FROM `spell_proc_event` WHERE `entry` IN (86508);
+INSERT INTO `spell_proc_event` VALUES (86508, 0x00, 0x03, 0x00000020, 0x00000000, 0x00000000, 0x00010000, 0x00000000, 0, 100, 0);
