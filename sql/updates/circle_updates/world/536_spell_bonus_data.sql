58b9d09360d89a36bf5a114d08fbf05f06431028
crawling <athreNd@hotmail.com>
2012-12-06 09:19:24 +0800

    [536] Ядро / Заклинания / Маг: Еще поправки к коэффициентам

---
+++ sql/updates/circle_updates/world/536_spell_bonus_data.sql
@@ -0,0 +1,6 @@
+-- Living Bomb
+UPDATE `spell_bonus_data` SET `direct_bonus`='0.515', `dot_bonus`='0.258' WHERE (`entry`='44457');
+UPDATE `spell_bonus_data` SET `comments`='Mage - Living Bomb' WHERE (`entry`='44457');
+-- Pyroblast
+UPDATE `spell_bonus_data` SET `direct_bonus`='1.545', `dot_bonus`='0.18' WHERE (`entry`='11366');
+UPDATE `spell_bonus_data` SET `direct_bonus`='1.545', `dot_bonus`='0.18' WHERE (`entry`='92315');
