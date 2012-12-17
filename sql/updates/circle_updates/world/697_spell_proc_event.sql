5d96e60d622f8a9100fda6c9bff8aceda3814db7
ungaminga <chloey@lavabit.com>
2012-12-17 10:04:03 +0200

    [697] Ядро / Заклинания / Охотник: Правки к таланту "Улучшеный верный выстрел" (Improved Steady Shot).

---
+++ sql/updates/circle_updates/world/697_spell_proc_event.sql
@@ -0,0 +1,4 @@
+-- Improved Steady Shot
+DELETE FROM `spell_proc_event` WHERE (`entry`='53221');
+DELETE FROM `spell_proc_event` WHERE (`entry`='53222');
+DELETE FROM `spell_proc_event` WHERE (`entry`='53224');
