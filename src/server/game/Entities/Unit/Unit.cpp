13b666d709c8bf3768da468a7fee4dbdbad6e191
ungaminga <chloey@lavabit.com>
2012-12-21 05:00:30 +0200

    [751] Ядро / Заклинания / Шаман: Исправлено количество бонусных атак у заклинания Оружие неистовства ветра (Windfury Weapon).

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -7541,8 +7541,8 @@ bool Unit::HandleDummyAuraProc(Unit* victim, uint32 damage, AuraEffect* triggere
                     if (cooldown)
                         player->AddSpellCooldown(dummySpell->Id, 0, time(NULL) + cooldown);
 
-                    // Attack Twice
-                    for (uint32 i = 0; i < 2; ++i)
+                    // attack three time
+                    for (uint32 i = 0; i < 3; ++i)
                         CastCustomSpell(victim, triggered_spell_id, &basepoints0, NULL, NULL, true, castItem, triggeredByAura);
 
                     return true;
