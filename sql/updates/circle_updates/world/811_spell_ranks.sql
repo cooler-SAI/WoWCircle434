669401d46ce841630260bc1e7b9e05f999340a90
ungaminga <chloey@lavabit.com>
2012-12-25 06:41:06 +0200

    [811] Ядро / Заклинания / Шаман: Исправлен талант "Земной захват" (Earth's Grasp).

---
+++ sql/updates/circle_updates/world/811_spell_ranks.sql
@@ -0,0 +1,4 @@
+-- Earth's Grasp
+
+INSERT INTO `spell_ranks` (`first_spell_id`, `spell_id`, `rank`) VALUES ('51483', '51483', '1');
+INSERT INTO `spell_ranks` (`first_spell_id`, `spell_id`, `rank`) VALUES ('51483', '51485', '2');
