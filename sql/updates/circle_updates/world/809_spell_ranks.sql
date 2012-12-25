4ebfaa6c3c1f9203f18298d8d998a12c92a61660
ungaminga <chloey@lavabit.com>
2012-12-25 03:52:03 +0200

    [809] Ядро / Заклинания / Разбойник: Исправлены правила стаков таланта "Воровская честь" (Honor Among Thieves).

---
+++ sql/updates/circle_updates/world/809_spell_ranks.sql
@@ -0,0 +1,5 @@
+-- Honor Among Thieves ranks staking
+
+INSERT INTO `spell_ranks` VALUES ('51698', '51698', '1');
+INSERT INTO `spell_ranks` VALUES ('51698', '51700', '2');
+INSERT INTO `spell_ranks` VALUES ('51698', '51701', '3');
