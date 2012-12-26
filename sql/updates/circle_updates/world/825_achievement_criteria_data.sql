ca9718bf20db82abfb31e57c49c0a4a9c2885ec0
Pelf <fry1988@mail.ru>
2012-12-26 19:50:45 +0300

    [825] Ядро / Достижения: исправлено достижение "Бука" для орды.

---
+++ sql/updates/circle_updates/world/825_achievement_criteria_data.sql
@@ -0,0 +1,3 @@
+DELETE FROM `achievement_criteria_data` WHERE `criteria_id`=5272 AND `type`=1;
+DELETE FROM `achievement_criteria_data` WHERE `criteria_id`=9838 AND `type`=1;
+DELETE FROM `achievement_criteria_data` WHERE `criteria_id`=9839 AND `type`=1;
