2f3d0940c8755c2661ce6adbab3900bb366afca0
Pelf <fry1988@mail.ru>
2013-01-04 16:35:44 +0300

    [950] База / Предметы: исправлен баг с предметом "Лопата".

---
+++ sql/updates/circle_updates/world/950_conditions.sql
@@ -0,0 +1,3 @@
+DELETE FROM `conditions` WHERE `SourceEntry`=89089;
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES 
+(17, 0, 89089, 0, 0, 31, 1, 3, 47872, 0, 0, 0, '', 'Shovel');
