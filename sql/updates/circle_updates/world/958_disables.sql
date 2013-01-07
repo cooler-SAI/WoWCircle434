ec929e6550e5c0b0c861f7d2c707572fe5ac1473
Heisenberg <Heisenberg@Heisenberg-0x40.Dlink>
2013-01-07 15:37:59 +0200

    [965] Ядро / Разное: Правки скула 958

--- sql/updates/circle_updates/world/958_disables.sql
+++ sql/updates/circle_updates/world/958_disables.sql
@@ -1,1 +1,3 @@
-INSERT INTO `disables` VALUE (0, 77361, 8, 0, 0, 'Temporary disable');
+DELETE FROM `disables` WHERE `entry` IN (77360, 77361) AND `sourceType` = 0;
+INSERT INTO `disables` VALUE (0, 77360, 2, 0, 0, 'Temporary disable');
+INSERT INTO `disables` VALUE (0, 77361, 2, 0, 0, 'Temporary disable');
