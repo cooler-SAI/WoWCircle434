974cd41dfc2609c401a6769566ee611f275fa789
Pelf <fry1988@mail.ru>
2013-01-07 18:34:49 +0400

    [966] База / Предметы: исправлен бонус 2T12 дд сета для воина.

---
+++ sql/updates/circle_updates/world/966_spell_proc_event.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_proc_event` WHERE `entry`=99234;
+INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES
+ (99234, 0, 4, 65536, 128, 0, 17408, 1027, 0, 0, 0);
