34d4a73689b0c07f40e09bd2d59f93e709e4b454
ungaminga <chloey@lavabit.com>
2013-01-08 21:29:14 +0200

    [987] Ядро / Заклинания / Воин: Исправлен "Символ ярости берсерка" (Glyph of Berserker Rage).

---
+++ sql/updates/circle_updates/world/987_spell_proc_event.sql
@@ -0,0 +1,4 @@
+-- (58096) Glyph of Berserker Rage
+DELETE FROM `spell_proc_event` WHERE `entry` IN (58096);
+INSERT INTO `spell_proc_event` VALUES (58096, 0x00, 0x04, 0x10000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0, 100, 0);
+
