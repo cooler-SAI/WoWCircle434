5b1a8bf835c5a265dd8d08909533af627e0a9b6b
crawling <athreNd@hotmail.com>
2013-01-11 04:02:42 +0800

    [1009] Ядро / Гильдии: исправлен запрос

--- src/server/game/Guilds/GuildMgr.cpp
+++ src/server/game/Guilds/GuildMgr.cpp
@@ -225,7 +225,7 @@ void GuildMgr::LoadGuilds()
                                                      //   23      24       25       26      27         28           29             30          
                                                      "c.name, c.level, c.class, c.zone, c.account, c.logout_time, week_rep, achievementPoints, "
                                                      //    31               32                33              34               35                36
-                                                     "FirstProffLevel, FirstProffSkill, FirstProffRank, SecondProffLevel, SecondProffSkill, SecondProffRank "
+                                                     "FirstProffLevel, FirstProffSkill, FirstProffRank, SecondProffLevel, SecondProffSkill, SecondProffRank, "
                                                      //    37              38
                                                      "total_activity, week_activity "
                                                      "FROM guild_member gm LEFT JOIN characters c ON c.guid = gm.guid ORDER BY guildid ASC");
