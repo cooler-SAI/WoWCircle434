4c0631487bbf155f7f896f1f05b6e2cf0975dfe9
crawling <athreNd@hotmail.com>
2013-01-12 12:07:11 +0800

    [1015]  Ядро / Заклинания: Ряд исправлений к иммунам, исправлено заклинание "Антимагический Панцирь" (Anti-Magic Shell)

--- src/server/game/Spells/SpellInfo.cpp
+++ src/server/game/Spells/SpellInfo.cpp
@@ -1338,10 +1338,17 @@ bool SpellInfo::CanPierceImmuneAura(SpellInfo const* aura) const
     if (Attributes & SPELL_ATTR0_UNAFFECTED_BY_INVULNERABILITY)
         return true;
 
+    if (aura && aura->Id == 33786)
+    {
+        // Re-cyclone & Fiery Fire
+        if (Id == aura->Id || SpellIconID == 109)
+            return false;
+    }
+
     // these spells (Cyclone for example) can pierce all...
     if ((AttributesEx & SPELL_ATTR1_UNAFFECTED_BY_SCHOOL_IMMUNE)
         // ...but not these (Divine shield for example)
-        && !(aura && (aura->Mechanic == MECHANIC_IMMUNE_SHIELD || aura->Mechanic == MECHANIC_INVULNERABILITY)))
+        && !(aura && (aura->Mechanic == MECHANIC_IMMUNE_SHIELD || aura->Mechanic == MECHANIC_INVULNERABILITY || aura->Id == 48707)))
         return true;
 
     return false;
