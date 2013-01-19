ad834768e0341d5087448c58f85f0afa4b1dadc7
ungaminga <chloey@lavabit.com>
2013-01-19 23:13:09 +0200

    [1081] Ядро / Система статов: Исправлена проблема с тем, что бафы которые увелчивали манареген визуально увеличивали и генерацию остальных типов маны.

--- src/server/game/Entities/Unit/StatSystem.cpp
+++ src/server/game/Entities/Unit/StatSystem.cpp
@@ -715,7 +715,7 @@ void Player::ApplyHealthRegenBonus(int32 amount, bool apply)
 
 void Player::UpdateManaRegen()
 {
-    if (getPowerType() == POWER_FOCUS)
+    if (getPowerType() != POWER_MANA)
         return;
 
     // Mana regen from spirit
