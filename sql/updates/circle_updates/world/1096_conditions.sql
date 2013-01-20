dcb5956ae8afbb9c8346a3c4c7bbf73d85f70cba
Pelf <fry1988@mail.ru>
2013-01-21 02:12:51 +0400

    [1096] База / Предметы: исправлен баг с предметом "Хвост утонувшей рокочущей ящерицы" (Drowned Thunder Lizard Tail).

---
+++ sql/updates/circle_updates/world/1096_conditions.sql
@@ -0,0 +1,3 @@
+DELETE FROM `conditions` WHERE `SourceEntry`=80962;
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `Comment`) VALUES
+('13', '1', '80962', '31', '0', '3', '43331', 'Drowned Thunder Lizard Tail');
