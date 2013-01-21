514e82c804e01448f2c87d6ec222a6abbcad90bd
Pelf <fry1988@mail.ru>
2013-01-21 19:31:52 +0300

    [1108] Ядро / Предметы: еще правки к комплектам:
     - Воин танк 2Т12,
     - Воин дпс 4Т12.

---
+++ sql/updates/circle_updates/world/1108_spell_proc_event.sql
@@ -0,0 +1,9 @@
+﻿-- Item  Warrior T12 Protection 2P Bonus
+DELETE FROM `spell_proc_event` WHERE `entry`=99239;
+INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES 
+(99239, 0, 4, 0, 512, 0, 16, 1027, 0, 100, 0);
+
+-- Item  Warrior T12 DPS 4P Bonus
+DELETE FROM `spell_proc_event` WHERE `entry`=99295;
+INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES 
+(99295, 0, 4, 33554432, 67117056, 0, 16, 1027, 0, 0, 0);
