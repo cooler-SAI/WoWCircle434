ca7fc32c3cfe8a957403d1f80d8ab999f56e83bd
ungaminga <chloey@lavabit.com>
2013-01-24 22:06:20 +0200

    [1141] Ядро / Заклинания / Жрец: Исправлен прок "Милость" (Grace).

---
+++ sql/updates/circle_updates/world/1141_spell_proc_event.sql
@@ -0,0 +1,7 @@
+-- (47516) Grace (Rank 1)
+DELETE FROM `spell_proc_event` WHERE `entry` IN (47516);
+INSERT INTO `spell_proc_event` VALUES (47516, 0x00, 0x06, 0x00001C00, 0x00010000, 0x00000000, 0x00004000, 0x00000000, 0, 100, 0);
+
+-- (47517) Grace (Rank 2)
+DELETE FROM `spell_proc_event` WHERE `entry` IN (47517);
+INSERT INTO `spell_proc_event` VALUES (47517, 0x00, 0x06, 0x00001C00, 0x00010000, 0x00000000, 0x00004000, 0x00000000, 0, 100, 0);
