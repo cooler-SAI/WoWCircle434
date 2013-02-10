336694c035ee6222a42877447b40e6512e86f0d6
Pelf <fry1988@mail.ru>
2013-02-10 14:41:34 +0300

    [1234] База / Разное: еще правка к очистке валюты.

--- sql/updates/4.06 to 4.3.4 migration/update_currency_3.sql
+++ sql/updates/4.06 to 4.3.4 migration/update_currency_3.sql
@@ -1,5 +1,5 @@
 -- Clear all valor and conquest points
-UPDATE `character_currency` SET `character_currency`.`total_count`=0 WHERE `character_currency`.`currency` IN (484, 483, 396, 390);
+DELETE FROM `character_currency` WHERE `character_currency`.`currency` IN (484, 483, 396, 390);
 
 -- Drop all arena teams
 TRUNCATE `arena_team`;
