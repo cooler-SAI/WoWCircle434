7bf68313512c49ac7d10e2b4faf47b25ee62c0fb
ungaminga <chloey@lavabit.com>
2013-02-12 13:50:46 +0200

    [1250] Ядро / Заклинания / Жрец: Правки к скалированию урона у "Кара" (Smite), "Врзыв разума" (Mind Blast) и "Священный огонь" (Holy Fire).

---
+++ sql/updates/circle_updates/world/1250_spell_bonus_data.sql
@@ -0,0 +1,3 @@
+UPDATE `spell_bonus_data` SET `direct_bonus`='0.856' WHERE (`entry`='585');                         -- Smite
+UPDATE `spell_bonus_data` SET `direct_bonus`='1.104' WHERE (`entry`='8092');                        -- Mind Blast
+UPDATE `spell_bonus_data` SET `direct_bonus`='1.11', `dot_bonus`='0.0312' WHERE (`entry`='14914');  -- Holy Fire
