534b467ec0ea788ea1761d77d66fb5f88bf98b2d
ungaminga <chloey@lavabit.com>
2013-02-26 17:10:11 +0200

    [1359] Ядро / Заклинания / Воин: Теперь "Вкус крови" (Taste for Blood) может прокать и в абсорб.

---
+++ sql/updates/circle_updates/world/1359_spell_proc_event.sql
@@ -0,0 +1,3 @@
+UPDATE `spell_proc_event` SET `procEx`='0' WHERE (`entry`='56636');
+UPDATE `spell_proc_event` SET `procEx`='0' WHERE (`entry`='56637');
+UPDATE `spell_proc_event` SET `procEx`='0' WHERE (`entry`='56638');
