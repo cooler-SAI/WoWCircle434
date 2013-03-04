c111a633bf1ca66760c2a4964e8dbb8303dd3934
ungaminga <chloey@lavabit.com>
2013-03-04 14:55:08 +0200

    [1392] Ядро / Заклинания / Чернокнижник: Правки к проку таланта "Mana Feed" (Магическая подпитка).

---
+++ sql/updates/circle_updates/world/1392_spell_proc_event.sql
@@ -0,0 +1,7 @@
+-- (85175) Mana Feed (Rank 2)
+DELETE FROM `spell_proc_event` WHERE `entry` IN (85175);
+INSERT INTO `spell_proc_event` VALUES (85175, 0x00, 0x00, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000002, 0, 0, 0);
+
+-- (30326) Mana Feed (Rank 1)
+DELETE FROM `spell_proc_event` WHERE `entry` IN (30326);
+INSERT INTO `spell_proc_event` VALUES (30326, 0x00, 0x00, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000002, 0, 0, 0);
