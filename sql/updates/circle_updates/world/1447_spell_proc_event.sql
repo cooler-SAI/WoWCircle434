d6a22a01d9d7fcd73dc663b998e4eece4e822723
Pelf <fry1988@mail.ru>
2013-03-10 18:44:59 +0300

    [1447] Ядро / Заклинания / Паладин: исправлены проки таланта "Обличение" (Denounce) и символа оглушающего броска щитом.

---
+++ sql/updates/circle_updates/world/1447_spell_proc_event.sql
@@ -0,0 +1,4 @@
+REPLACE INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES
+(56414, 0, 10, 16384, 0, 0, 65792, 1027, 0, 100, 0),
+(31825, 0, 10, 0, 2, 0, 0, 0, 0, 0, 0),
+(85510, 0, 10, 0, 2, 0, 0, 0, 0, 0, 0);
