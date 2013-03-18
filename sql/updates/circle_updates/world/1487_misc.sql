d617c2cc5da31dfcf318a58479f4fd13aa97535c
Pelf <fry1988@mail.ru>
2013-03-18 23:57:28 +0300

    [1487] Скрипты / Зул'Фаррак:
     - исправлена атакуемость босса Вождь Укорз Песчаный Череп,
     - исправлена атакуемость босса Уруузлу,
     - исправлена дверь к последним боссам.

---
+++ sql/updates/circle_updates/world/1487_misc.sql
@@ -0,0 +1,3 @@
+﻿UPDATE `gameobject_template` SET `faction`=0 WHERE `entry`=146084;
+DELETE FROM `gameobject` WHERE `guid`=189741 AND `id`=146084;
+UPDATE `creature_template` SET `unit_flags`=0 WHERE `entry` IN (7267, 7797);
