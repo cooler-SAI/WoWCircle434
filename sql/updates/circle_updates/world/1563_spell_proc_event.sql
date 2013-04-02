572949d676bbc5263d784249ef8ddc8133964400
ungaminga <chloey@lavabit.com>
2013-04-02 14:30:14 +0300

    [1563] Ядро / Заклинания / Рыцарь смерти: Добавлен забытый sql к "Темный симулякр" (Dark Simulacrum).

---
+++ sql/updates/circle_updates/world/1563_spell_proc_event.sql
@@ -0,0 +1,6 @@
+-- (77606) Dark Simulacrum
+DELETE FROM `spell_proc_event` WHERE `entry` IN (77606);
+INSERT INTO `spell_proc_event` VALUES (77606, 0x00, 0x00, 0x00000000, 0x00000000, 0x00000000, 0x00015400, 0x00010000, 0, 101, 60);
+-- (77616) Dark Simulacrum
+DELETE FROM `spell_proc_event` WHERE `entry` IN (77616);
+INSERT INTO `spell_proc_event` VALUES (77616, 0x00, 0x00, 0x00000000, 0x00000000, 0x00000000, 0x00015400, 0x00010000, 0, 101, 0);
