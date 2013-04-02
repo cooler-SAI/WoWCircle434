f6129620e94297d1ff0da9bb99fef71b952a303a
ungaminga <chloey@lavabit.com>
2013-04-02 22:06:49 +0300

    [1567] Ядро / Заклинания / Чернокнижник: Теперь заклинание "Демонический круг" (Demonic Circle) не снимает root-эффекты.

--- src/server/scripts/Spells/spell_warlock.cpp
+++ src/server/scripts/Spells/spell_warlock.cpp
@@ -459,7 +459,6 @@ class spell_warl_demonic_circle_teleport : public SpellScriptLoader
                     if (GameObject* circle = player->GetGameObject(WARLOCK_DEMONIC_CIRCLE_SUMMON))
                     {
                         player->NearTeleportTo(circle->GetPositionX(), circle->GetPositionY(), circle->GetPositionZ(), circle->GetOrientation());
-                        player->RemoveMovementImpairingAuras();
                     }
                 }
             }
