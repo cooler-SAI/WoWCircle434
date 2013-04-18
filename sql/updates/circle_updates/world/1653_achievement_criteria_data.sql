87848ad4b01b02a1245c5b688c80323d31af66e1
ungaminga <chloey@lavabit.com>
2013-04-18 18:32:38 +0400

    [1655] typo.

---
+++ sql/updates/circle_updates/world/1653_achievement_criteria_data.sql
@@ -0,0 +1,3 @@
+REPLACE INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES ('-6229', 'spell_warlock_nether_protection_trigger');
+REPLACE INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES ('-91711', 'spell_warlock_nether_protection_trigger');
+REPLACE INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES ('-7812', 'spell_warlock_nether_protection_trigger');
