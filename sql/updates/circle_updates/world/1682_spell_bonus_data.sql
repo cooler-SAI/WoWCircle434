bc0ae2fe1f9f54caf54a98f47b6276347d3eb785
Pelf <fry1988@mail.ru>
2013-04-24 11:20:47 +0300

    [1682] База / Предметы: исправлен баг с предметом "Подвеска из животворного рубина".

---
+++ sql/updates/circle_updates/world/1682_spell_bonus_data.sql
@@ -0,0 +1,3 @@
+REPLACE INTO `spell_bonus_data` (`entry`, `direct_bonus`, `dot_bonus`, `ap_bonus`, `ap_dot_bonus`, `comments`) VALUES 
+(31024, 0, 0, 0, 0, 'Living Ruby Pendant'),
+(25608, 0, 0, 0, 0, 'Citrine Pendant of Golden Healing');
