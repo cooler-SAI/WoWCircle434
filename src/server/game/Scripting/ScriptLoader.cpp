5cae2eee7215cd7c93dbd3152b4d769cb18137a6
kores <devnull@localhost>
2013-04-26 17:33:54 +0600

    add ragnaros script

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -696,6 +696,7 @@ void AddSC_boss_bethtilac();
 void AddSC_boss_alysrazor();
 void AddSC_boss_baleroc();
 void AddSC_boss_majordomo_staghelm();
+void AddSC_boss_ragnaros_cata();
 
 void AddSC_instance_end_time(); // End Time
 void AddSC_end_time();
@@ -1438,6 +1439,7 @@ void AddNorthrendScripts()
     AddSC_boss_alysrazor();
     AddSC_boss_baleroc();
     AddSC_boss_majordomo_staghelm();
+    AddSC_boss_ragnaros_cata();
 
     AddSC_instance_end_time(); // End Time
     AddSC_end_time();
--- src/server/scripts/EasternKingdoms/Firelands/firelands.h
+++ src/server/scripts/EasternKingdoms/Firelands/firelands.h
@@ -1,6 +1,7 @@
 #ifndef DEF_FIRELANDS_H
 #define DEF_FIRELANDS_H
 
+#include "ObjectMgr.h"
 #define FLScriptName "instance_firelands"
 
 enum Data
@@ -14,6 +15,10 @@ enum Data
     DATA_RAGNAROS   = 6,
     DATA_RIPLIMB    = 7,
     DATA_RAGEFACE   = 8,
+    
+    // ragnaros from uwow
+    DATA_INTRO_EVENT,
+    DATA_RAGNAROS_EVENT,
 };
 
 enum CreatureIds
@@ -26,7 +31,12 @@ enum CreatureIds
     NPC_ALYSRAZOR                   = 52530, 
     NPC_BALEROC                     = 53494,
     NPC_STAGHELM                    = 52571,
-    NPC_RAGNAROS                    = 52409,
+    NPC_RAGNAROS_CATA              = 52409,
+	
+    //other npc's
+    NPC_SULFURAS            = 53420,    //ragnaros boss weapon
+    NPC_LIVING_METEOR       = 53500,
+    NPC_MOLTEN_ELEMENTAR    = 53189,
     
     // alysrazor event
     NPC_BLAZING_MONSTROSITY_LEFT    = 53786,
@@ -35,6 +45,20 @@ enum CreatureIds
     NPC_HARBINGER_OF_FLAME          = 53793,
     NPC_MOLTEN_EGG_TRASH            = 53914,
     NPC_SMOULDERING_HATCHLING       = 53794, 
+	
+    // Ragnaros from uwow
+    NPC_ENGULFING_FLAMES_TRIGGER    = 53485,
+    NPC_SPLITTING_BLOW_TRIGGER      = 53393,
+    NPC_MAGMA_POOL_TRIGGER          = 53729,
+    NPC_PLATFORM_TRIGGER            = 53952,
+    NPC_MAGMA_TRAP                  = 53086,
+    NPC_SULFURAS_HAMMER             = 53420,
+    NPC_SULFURAS_FLAME_WALL         = 38327,
+    NPC_SULFURAS_SMASH_TARGET_1     = 53268,
+    NPC_LAVA_WAVE                   = 53363,
+    NPC_SON_OF_FLAME                = 53140,
+    NPC_MOLTEN_SEED                 = 53186,
+    NPC_LAVA_SCION                  = 53231,
 };
 
 enum GameobjectIds
@@ -58,6 +82,24 @@ enum GameobjectIds
     GO_EMBERSTONE_FRAGMENT      = 209035,
     GO_PYRESHELL_FRAGMENT       = 209036,
     GO_OBSIDIAN_FLECKED_CHITIN  = 209037,
+};
+
+enum MovePoints
+{
+    POINT_HAMMER,
+    POINT_RAGNAROS_DOWN,         //end of each phase
+    POINT_RAGNAROS_UP,           //start of each next phase
+    POINT_RAGNAROS_STANDUP,      //only on heroic mode
+    POINT_SULFURAS_SMASH,        //target for smashes
+};
+
+enum Actions
+{
+    //Ragnaros
+    ACTION_RAGNAROS_DOWN,        //end of each phase
+    ACTION_RAGNAROS_UP,          //start of each next phase
+    ACTION_RAGNAROS_STANDUP,     //only on heroic mode
+    ACTION_SULFURAS_SMASH,       //target for smashes
 };
 
 #endif
