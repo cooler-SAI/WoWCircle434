14991b769249ba3ccc29c2e5c333a10194f010a6
ungaminga <chloey@lavabit.com>
2013-05-04 22:14:55 +0300

    [1726] Ядро / Заклинания / Чернокнижник: Исправлен бонус периодинческого урона от заклинания "Пламя Тьмы" (Shadowflame).

---
+++ sql/updates/circle_updates/world/1725_spell_proc_event.sql
@@ -0,0 +1,3 @@
+-- (56229) Glyph of Shadowburn
+DELETE FROM `spell_proc_event` WHERE `entry` IN (56229);
+INSERT INTO `spell_proc_event` VALUES (56229, 0x00, 0x05, 0x00000080, 0x00000000, 0x00000000, 0x00010000, 0x00000000, 0, 100, 0);
