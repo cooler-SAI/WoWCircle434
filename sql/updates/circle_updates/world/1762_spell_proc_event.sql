a0d25f7e9863ef14d47752e0983aa847a86300be
Pelf <fry1988@mail.ru>
2013-05-19 01:37:30 +0300

    [1762] Ядро / Заклинания / Чернокнижник: исправлен прок таланта "Магическая подпитка".

---
+++ sql/updates/circle_updates/world/1762_spell_proc_event.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_proc_event` WHERE `entry` IN (30326, 85175);
+REPLACE INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES 
+(91702, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0);
