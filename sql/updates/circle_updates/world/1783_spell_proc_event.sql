86cc06d23f808f25fd84b4e0b8e6102261cb42fa
ungaminga <chloey@lavabit.com>
2013-05-21 23:53:55 +0300

    [1783] Ядро / Предметы / Шаман: Исправлен 2/5 T13 бонус шамана (Elemental Mastery).

---
+++ sql/updates/circle_updates/world/1783_spell_proc_event.sql
@@ -0,0 +1,4 @@
+-- (105780) Item - Shaman T13 Elemental 2P Bonus (Elemental Mastery)
+DELETE FROM `spell_proc_event` WHERE `entry` IN (105780);
+INSERT INTO `spell_proc_event` VALUES (105780, 0x00, 0x0B, 0x00000000, 0x00004000, 0x00000000, 0x00000000, 0x00000000, 0, 100, 0);
+
