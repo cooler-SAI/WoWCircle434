5f9f6f6868e882149a645cf678ec8ba425716072
ungaminga <chloey@lavabit.com>
2013-06-06 20:45:51 +0300

    [1904] Ядро / Заклинания / Жрец: Исправлен стак заклинания "Защита от темной магии" (Shadow Protection).

---
+++ sql/updates/circle_updates/world/1904_spell_stack_rules.sql
@@ -0,0 +1,2 @@
+REPLACE INTO `spell_group` (`id`, `spell_id`, `comments`) VALUES ('2122', '79106', 'Shadow Protection'), ('2122', '79107', 'Shadow Protection raid');
+REPLACE INTO `spell_group_stack_rules` (`group_id`, `stack_rule`) VALUES ('2122', '1');
