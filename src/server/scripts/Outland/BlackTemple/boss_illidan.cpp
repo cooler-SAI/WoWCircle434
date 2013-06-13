431f61db969605ee72571b89381db970f3c1df25
ANTOMA <devnull@localhost>
2013-06-13 20:34:37 +0300

    [I-core]fix event start "Akama" (BlackTemple)

--- src/server/scripts/Outland/BlackTemple/boss_illidan.cpp
+++ src/server/scripts/Outland/BlackTemple/boss_illidan.cpp
@@ -1531,8 +1531,11 @@ public:
             if (!JustCreated)
                 return;
             float x, y, z;
-            if (GETGO(Gate, GateGUID))
-                Gate->GetPosition(x, y, z);
+            //if (GETGO(Gate, GateGUID)) // fuck up after new invis system
+            //    Gate->GetPosition(x, y, z);
+             GateGUID = pInstance->GetData64(DATA_GAMEOBJECT_ILLIDAN_GATE);
+ 	if (GameObject* obj = pInstance->instance->GetGameObject(GateGUID))
+                 obj->GetPosition(x, y, z);
             else
                 return; // if door not spawned, don't crash server
 
