780fa3549a964fdb013f6319e2f1d9e163bbeb9a
kores <devnull@localhost>
2013-06-26 22:19:59 +0600

    Implemented a command to list hostile units and enclosed these commands in try { ... } catch block to prevent possible exceptions.

--- sql/isengard/world_commands.sql
+++ sql/isengard/world_commands.sql
@@ -4,3 +4,9 @@ INSERT INTO `trinity_string` (`entry`, `content_default`, `content_loc1`, `conte
 DELETE FROM `command` WHERE `name` LIKE 'npc near';
 INSERT INTO `command` (`name`, `security`, `help`) VALUES
 ('npc near', 5, 'Syntax: .npc near  [#distance]\r\n\r\nOutput npc at distance #distance from player. Output npc guids and coordinates sorted by distance from character. If #distance not provided use 10 as default value.');
+DELETE FROM `command` WHERE `name`='list aggro';
+INSERT INTO `command` (`name`, `security`, `help`) VALUES
+('list aggro', 1, 'Syntax: .list aggro\r\n\r\n Lists all units currently attacking the selected unit (or you, if none selected) and their threat.');
+DELETE FROM `command` WHERE `name`='list hostiles';
+INSERT INTO `command` (`name`, `security`, `help`) VALUES
+('list hostiles', 1, 'Syntax: .list hostiles\r\n\r\n Lists all units currently hostile towards the selected unit (or you, if none selected) and their threat.');
