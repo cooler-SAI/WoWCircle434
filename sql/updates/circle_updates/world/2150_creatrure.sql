dc07b2ba1665460cd979c7c0a8164472da1a9aae
Pelf <fry1988@mail.ru>
2013-08-05 08:13:46 +0400

    [2150] База / Квесты: дополнение к [2138].

---
+++ sql/updates/circle_updates/world/2150_creatrure.sql
@@ -0,0 +1,3 @@
+﻿DELETE FROM `creature` WHERE `id`=57777;
+INSERT INTO `creature` (`id`, `map`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`) VALUES 
+(57777, 0, 1, 1, 0, 0, 4.61503, -1622.89, 196.652, 6.25569, 300, 0, 0, 387450, 0, 0, 0, 0, 0);
