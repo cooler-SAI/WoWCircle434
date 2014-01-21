60b287871ab64aa4b9e7d08400b6d71b6cbb9e5c
Rubaka <blackmanos@yandex.ru>
2014-01-22 01:09:54 +0200

    Lunar Invitation

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -1680,6 +1680,11 @@ void Spell::EffectDummy(SpellEffIndex effIndex)
 
     switch (m_spellInfo->Id)
     {
+        // Lunar Invitation
+        case 26373:
+            if(m_caster->ToPlayer())
+                m_caster->ToPlayer()->TeleportTo(1, 7581.144531f, -2211.47900f, 473.639771f, 0, 0);
+        break;
         case 61316:     // Dalaran Brilliance
         {
             // kinda exception
