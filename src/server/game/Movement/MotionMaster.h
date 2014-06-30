514f58f56db15521d22bd3100e1c5d27ce3bc2c4
mojito-ice <akriso@i.ua>
2014-07-01 02:48:25 +0400

    [2665]  let MoveJump read Position const

--- src/server/game/Movement/MotionMaster.h
+++ src/server/game/Movement/MotionMaster.h
@@ -164,6 +164,8 @@ class MotionMaster //: private std::stack<MovementGenerator *>
         void MoveKnockbackFrom(float srcX, float srcY, float speedXY, float speedZ);
         void MoveJumpTo(float angle, float speedXY, float speedZ);
         void MoveJump(float x, float y, float z, float speedXY, float speedZ, float o = 10.0f, uint32 id = EVENT_JUMP);
+        void MoveJump(Position const& pos, float speedXY, float speedZ, uint32 id = EVENT_JUMP)
+        { MoveJump(pos.m_positionX, pos.m_positionY, pos.m_positionZ, speedXY, speedZ, id); };
         void CustomJump(float x, float y, float z, float speedXY, float speedZ, uint32 id = 0);
         void MoveFall(uint32 id = 0);
 
