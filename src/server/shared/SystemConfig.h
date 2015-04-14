2c1f3f672abc925e536b1c1052e24b7525f7519c
Shuni <wowa.ru29@gmail.com>
2015-04-14 16:25:37 +0300

    [EC-3]*Небольшие правки по копирайтам.

--- src/server/shared/SystemConfig.h
+++ src/server/shared/SystemConfig.h
@@ -24,7 +24,7 @@
 #include "Define.h"
 #include "revision.h"
 
-#define _PACKAGENAME "CerberProject Core"
+#define _PACKAGENAME "Core:EWCore\nBased on: EWTDB\nMain Developer: Shuni \n"
 
 #if CERBERCORE_ENDIAN == CERBERCORE_BIGENDIAN
 # define _ENDIAN_STRING "big-endian"
