a57519566d9596626c8e22d336058e3170e3d661
Shuni <wowa.ru29@gmail.com>
2015-04-17 01:20:11 +0300

    [EC-13]*Исправление отображения ревизии

--- revision.h.in.cmake
+++ revision.h.in.cmake
@@ -1,11 +1,12 @@
 #ifndef __REVISION_H__
 #define __REVISION_H__
- #define _HASH                      "@rev_hash@"
- #define _DATE                      "@rev_date@"
+ #define _HASH                      "ed06f56e3c6a4ff4cb24a97684784d730e8c2715"
+ #define _DATE                      "17.04.2014"
+ #define _BRANCH                    "master"
  #define VER_COMPANYNAME_STR        "TrinityCore Developers"
- #define VER_LEGALCOPYRIGHT_STR     "(c)2008-2012 TrinityCore"
+ #define VER_LEGALCOPYRIGHT_STR     "(c)2008-2015 TrinityCore"
  #define VER_FILEVERSION            0,0,0
- #define VER_FILEVERSION_STR        "@rev_date@ (@rev_hash@)"
+ #define VER_FILEVERSION_STR        "ed06f56e3c6a4ff4cb24a97684784d730e8c2715 master"
  #define VER_PRODUCTVERSION         VER_FILEVERSION
  #define VER_PRODUCTVERSION_STR     VER_FILEVERSION_STR
 #endif // __REVISION_H__
