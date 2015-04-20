4f2271c54d366a6753e988e7252dce3a41959355
Shuni <wowa.ru29@gmail.com>
2015-04-20 23:30:29 +0300

    [EC-20]*Обновление ревизии БД,добавит бат файл,перенес все скулы в 1.

--- sql/updates/world/update_error.sql
+++
@@ -1,9 +0,0 @@
--- Fix spell script name on startup error
-INSERT INTO `spell_script_names` VALUES
-(66299,'spell_radio'),
-(83883,'spell_q26861_door_dummy'),
-(19593,'spell_egg_explosion');
-
--- Исправление битых НИП
-UPDATE `creature_template` SET `npcflag` = 0 WHERE `entry` = 34865;
-UPDATE `creature_template` SET `unit_flags` = 1 WHERE `entry` = 35120;
