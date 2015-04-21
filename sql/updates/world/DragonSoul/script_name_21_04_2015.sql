7a5310b26d1ced50571892c2f7f7dca1dc42b74e
Eldro Gaming <diavolb@gmail.com>
2015-04-21 04:37:08 +0300

    [EC-24]*Привязка скриптнеймов в Душе Дракона

---
+++ sql/updates/world/DragonSoul/script_name_21_04_2015.sql
@@ -0,0 +1,3 @@
+UPDATE `creature_template` SET `ScriptName` = 'npc_dragon_soul_teleport' WHERE `entry` = 57287;
+UPDATE `creature_template` SET `ScriptName` = 'npc_dragon_soul_teleport' WHERE `entry` = 57288;
+UPDATE `creature_template` SET `ScriptName` = 'npc_dragon_soul_teleport' WHERE `entry` = 57289;
